4681c8bd0e9f45634bb7bbee3ab530b56f87ef74
PetrPestrikov <60996371+PetrPestrikov@users.noreply.github.com>
2021-09-01 04:56:43 +0300

    Add additional comments to the code

--- data_structure/fifo/FIFO.h
+++ data_structure/fifo/FIFO.h
@@ -22,17 +22,17 @@
  **********************/
 
 typedef  void(*setCritFunc_t)(void);		//a pointer to a function that sets the critical section
-typedef  void(*relCritFunc_t)(void);	    //a pointer to a function that releases the critical section
+typedef  void(*relCritFunc_t)(void);		//a pointer to a function that releases the critical section
 
 /*****************************************************
  * Describing a generic FIFO ring buffer
  * use a separate bit to track the case of data loopback.
  *************************************************** */
 typedef struct fifo_t{
-	uint16_t size;					// buffer size
+	uint16_t size;				// buffer size
 	uint16_t fifoReadIndex;			//points to the element to be read next (where the fresh data is) a pointer to the beginning of the data
 	uint16_t fifoWriteIndex;		//Indicates the item that was last written
-    uint8_t	 msb;					//bit that determines whether the buffer has been overwritten
+    uint8_t	 msb;				//bit that determines whether the buffer has been overwritten
     void *   masHeadptr;			//pointer to the first element of the array
 	uint8_t  dataTypeLen;			//Length of the data field in one FIFO cell in bytes
 	setCritFunc_t setCritFoo;		//a pointer to a function that sets the critical section
@@ -43,12 +43,12 @@ typedef struct fifo_t{
  *	functions for working with FIFO
  *************************************************/
 void createFifo(fifo_t *fifo, uint16_t ArrSize, void * Headptr, uint8_t DataTypeLen);	//create buffer
-void writeFifo (fifo_t *fifo, void * data);												//writing to the buffer by reference
-void  * readFifo (fifo_t *fifo);														//reading
-void flushFifo(fifo_t *fifo);															//buffer erase
-bool isEmptyFifo(fifo_t *fifo);															//returns true if the buffer is empty
-bool isFullFifo(fifo_t *fifo);															//returns true if the buffer is full
-uint16_t getBufferCount(fifo_t *fifo);													// the current number of elements written in the buffer
+void writeFifo (fifo_t *fifo, void * data);						//writing to the buffer by reference
+void  * readFifo (fifo_t *fifo);							//reading
+void flushFifo(fifo_t *fifo);								//buffer erase
+bool isEmptyFifo(fifo_t *fifo);								//returns true if the buffer is empty
+bool isFullFifo(fifo_t *fifo);								//returns true if the buffer is full
+uint16_t getBufferCount(fifo_t *fifo);							// the current number of elements written in the buffer
 
 
 
